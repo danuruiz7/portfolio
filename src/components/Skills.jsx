10d55b9a5a0a2a4a4fbec3bc252d18f8a0eac958
Daniel Ruiz <danuruiz7@hotmail.com>
2023-07-25 19:21:51 +0200

    maquetando y dandole estilo al apartado skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import "../styles/Skills.css";
+import { skillsFront, skillsBack, tools, dataBase } from "../info/habilidades";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section className="content">
+      <h1 className="title-skills">Habilidades</h1>
+      <div className="container-skills">
+        <div className="card-skills">
+          <h3 className="title-card">Frontend</h3>
+          <ul className="list-skills">
+            {skillsFront.map((skill) => (
+              <li className="skill" key={skill.name}>
+                <img src={skill.url} alt={skill.name} />
+                <p>{skill.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="card-skills">
+          <h3 className="title-card">Backend</h3>
+          <ul className="list-skills">
+            {skillsBack.map((skill) => (
+              <li className="skill" key={skill.name}>
+                <img src={skill.url} alt={skill.name} />
+                <p>{skill.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="card-skills">
+          <h3 className="title-card">Tools</h3>
+          <ul className="list-skills">
+            {tools.map((skill) => (
+              <li className="skill" key={skill.name}>
+                <img src={skill.url} alt={skill.name} />
+                <p>{skill.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="card-skills">
+          <h3 className="title-card">Data Base</h3>
+          <ul className="list-skills">
+            {dataBase.map((skill) => (
+              <li className="skill" key={skill.name}>
+                <img src={skill.url} alt={skill.name} />
+                <p>{skill.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
---
+++ src/info/habilidades.js
@@ -0,0 +1,55 @@
+export const skillsFront = [
+  {
+    url: "https://github.com/jmontes33/React-Portfolio/blob/main/src/assets/html5.png?raw=true",
+    name: "HTML",
+  },
+  {
+    url: "https://github.com/jmontes33/React-Portfolio/blob/main/src/assets/css3.png?raw=true",
+    name: "CSS",
+  },
+  {
+    url: "https://github.com/jmontes33/React-Portfolio/blob/main/src/assets/javascript.png?raw=true",
+    name: "JS",
+  },
+  {
+    url: "https://raw.githubusercontent.com/jmontes33/React-Portfolio/main/src/assets/react.png",
+    name: "React",
+  },
+];
+
+export const skillsBack = [
+  {
+    url: "https://github.com/jmontes33/React-Portfolio/blob/main/src/assets/nodejs.png?raw=true",
+    name: "Node.js",
+  },
+  {
+    url: "https://www.edureka.co/blog/wp-content/uploads/2019/07/express-logo.png",
+    name: "Express",
+  },
+];
+
+export const tools = [
+  {
+    url: "https://github.com/jmontes33/React-Portfolio/blob/main/src/assets/git.png?raw=true",
+    name: "Git",
+  },
+  {
+    url: "https://github.com/jmontes33/React-Portfolio/blob/main/src/assets/github.png?raw=true",
+    name: "Github",
+  },
+  {
+    url: "https://github.com/jmontes33/React-Portfolio/blob/main/src/assets/npm.png?raw=true",
+    name: "NPM",
+  },
+];
+
+export const dataBase = [
+  {
+    url: "https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_logo_icon_146424.png",
+    name: "MongoDB",
+  },
+  {
+    url: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png",
+    name: "MySQL",
+  },
+];
