8475d722e0155b4ebd9012e8ad14b7cb8f7bce75
Daniel Ruiz <danuruiz7@hotmail.com>
2023-07-26 19:21:25 +0200

    agregando nuevo proyecto quiz app y refactorizando un poco el codigo

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,38 +1,5 @@
 import "../styles/Proyectos.css";
-import previewImg from "../img/preview.png";
-
-const proyectos = [
-  {
-    title: "Wheather App",
-    description:
-      "Proyecto realizado con React JS, utilizando el método fetch para llamar a la API de WeatherAPI y obtener datos meteorológicos en tiempo real.",
-    techs: "React JS, Css, API",
-    image:
-      "https://raw.githubusercontent.com/danuruiz7/WeatherApp/main/public/preview-wahd.png",
-    code: "https://github.com/danuruiz7/WeatherApp",
-    preview: "https://weather-app-danudev.vercel.app",
-  },
-  {
-    title: "Lira Motors",
-    description:
-      "Pagina web totalmente responsive creada con HTML, CSS, JavaScript para un taller mecanico real.",
-    techs: "HTML, CSS, JavaScript",
-    image:
-      "https://github.com/danuruiz7/liramotorsweb/blob/main/img/preview.png?raw=true",
-
-    code: "https://github.com/danuruiz7/liramotorsweb",
-
-    preview: "https://liramotors.vercel.app/",
-  },
-  {
-    title: "Portfolio",
-    description: "Portfolio personal para mostrar mis proyectos y experiencias",
-    techs: "React JS, React Router 6, Css",
-    image: previewImg,
-    code: "https://github.com/danuruiz7/portfolio/tree/main",
-    preview: "#",
-  },
-];
+import proyectos from "../info/proyectos";
 
 const Proyectos = () => {
   return (
---
+++ src/info/proyectos.js
@@ -0,0 +1,46 @@
+import previewImg from "../img/preview.png";
+
+const proyectos = [
+  {
+    title: "Wheather App",
+    description:
+      "Proyecto realizado con React JS, utilizando el método fetch para llamar a la API de WeatherAPI y obtener datos meteorológicos en tiempo real.",
+    techs: "React JS, Css, API",
+    image:
+      "https://raw.githubusercontent.com/danuruiz7/WeatherApp/main/public/preview-wahd.png",
+    code: "https://github.com/danuruiz7/WeatherApp",
+    preview: "https://weather-app-danudev.vercel.app",
+  },
+  {
+    title: "Lira Motors",
+    description:
+      "Pagina web totalmente responsive creada con HTML, CSS, JavaScript para un taller mecanico real.",
+    techs: "HTML, CSS, JavaScript",
+    image:
+      "https://github.com/danuruiz7/liramotorsweb/blob/main/img/preview.png?raw=true",
+
+    code: "https://github.com/danuruiz7/liramotorsweb",
+
+    preview: "https://liramotors.vercel.app/",
+  },
+  {
+    title: "Portfolio",
+    description: "Portfolio personal para mostrar mis proyectos y experiencias",
+    techs: "React JS, React Router 6, Css",
+    image: previewImg,
+    code: "https://github.com/danuruiz7/portfolio/tree/main",
+    preview: "#",
+  },
+  {
+    title: "Quiz App",
+    description:
+      "App de trivias con distintas categorias para seleccionar ven a probarlas todas...!!!",
+    techs: "React JS, React Router 6, Css",
+    image:
+      "https://github.com/danuruiz7/quizapp/blob/main/public/preview.png?raw=true",
+    code: "https://github.com/danuruiz7/quizapp",
+    preview: "https://quizapp-dr.vercel.app/",
+  },
+];
+
+export default proyectos;
