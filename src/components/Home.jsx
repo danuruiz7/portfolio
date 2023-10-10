38d739ed4e436924ca902b1679d459d9c4228de6
Daniel Ruiz <danuruiz7@hotmail.com>
2023-10-10 15:26:04 +0200

    Actualizando descripcion principal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,10 +13,12 @@ const Home = () => {
             <p className="card-description">
               Desarrollador Full Stack con experiencia en Front-end utilizando
               tecnologías como <strong>HTML, CSS, JavaScript y React,</strong>{' '}
-              incluyendo TypeScript. En el Back-end, tengo sólidos conocimientos
-              en Node.js, Express.js y Python con Django Rest Framework, además
-              de experiencia en bases de datos como PostgreSQL y MySQL. También
-              soy competente en el uso de Git y GitHub para el control de
+              incluyendo <strong>TypeScript</strong>. En el Back-end, tengo
+              sólidos conocimientos en <strong>Node.js, Express.js</strong> y{' '}
+              <strong>Python con Django Rest Framework</strong>, además de
+              experiencia en bases de datos{' '}
+              <strong>SQL (PostgreSQL y MySQL)</strong>. También soy competente
+              en el uso de <strong> Git y GitHub</strong> para el control de
               versiones y colaboración en equipo. Con casi un año de
               experiencia, he adquirido la mayoría de mis habilidades de forma
               autodidacta. Estoy comprometido con la excelencia en el desarrollo
