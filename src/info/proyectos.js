ce81328c82a76a7d04fb7559417384f41479b585
Daniel Ruiz <danuruiz7@hotmail.com>
2023-11-15 23:13:15 +0100

    agregado el clonespotify

--- src/info/proyectos.js
+++ src/info/proyectos.js
@@ -12,6 +12,16 @@ const proyectos = [
     code: 'https://github.com/danuruiz7/tasksApp',
     preview: 'https://tasks-app-by-danuruiz7.vercel.app/',
   },
+  //TaskAPP
+  {
+    title: 'ClonSpotify',
+    description:
+      'Crea tu cuenta, organiza tareas y accede desde cualquier dispositivo al instante.',
+    techs: 'Astro,ReactTS',
+    image: '/src/img/spoti.png',
+    code: 'https://github.com/danuruiz7/ClonSpotify',
+    preview: 'https://clon-spotify-two.vercel.app/',
+  },
   //Quiz App
   {
     title: 'Quiz App',
